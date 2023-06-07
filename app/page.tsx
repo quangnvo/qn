fa63de1420f26a9681e5878526b4a5fe33a964a5
quangnvo <voquang.usth@gmail.com>
2023-06-07 23:30:55 +0800

    modify background

--- app/page.tsx
+++ app/page.tsx
@@ -115,7 +115,7 @@ export default function Home() {
 					</div> */}
 
 					{/* Background grid */}
-					<div className="absolute inset-x-0 top-0 h-96 rotate-180 text-gray-500/20 opacity-60 [mask-image:linear-gradient(to_bottom,transparent,white)]">
+					{/* <div className="absolute inset-x-0 top-0 h-96 rotate-180 text-gray-500/20 opacity-60 [mask-image:linear-gradient(to_bottom,transparent,white)]">
 						<svg className="absolute inset-0 h-full w-full" xmlns="http://www.w3.org/2000/svg">
 							<defs>
 								<pattern id="grid-pattern" width={32} height={32} patternUnits="userSpaceOnUse" x="50%" y="100%" patternTransform="translate(0 -1)">
@@ -123,7 +123,7 @@ export default function Home() {
 							</defs>
 							<rect width="100%" height="100%" fill="url(#grid-pattern)" />
 						</svg>
-					</div>
+					</div> */}
 
 
 					{/* Text */}
