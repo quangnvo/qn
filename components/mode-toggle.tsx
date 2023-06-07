d850a7ebf9d0fbd96e6d62d69bce1f5312ca06d7
quangnvo <voquang.usth@gmail.com>
2023-06-07 18:57:58 +0800

    modify dark mode button

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -2,12 +2,25 @@
 
 import { useTheme } from "next-themes"
 import { MoonIcon, SunIcon } from "./ui/icons-sun-moon"
+import { useEffect, useState } from "react"
 
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
 
-  let theme = localStorage.getItem("theme")
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    // return null
+    return (
+      <button className="border rounded-md w-10 h-10 flex items-center justify-center bg-white dark:bg-dark dark:text-white">
+        ...
+      </button>
+    )
+  }
 
   return (
     <button onClick={() => {
