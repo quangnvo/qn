9af2e5a5e83f220db26061a0128ffb40a18365bc
quangnvo <voquang.usth@gmail.com>
2023-12-07 11:05:57 +0800

    add the function for backtotop button without using any library

--- components/BackToTop.tsx
+++ components/BackToTop.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+"use client"
+
 import { Button } from "@/components/ui/button"
 
-
 const BackToTop = () => {
-    return (
-        <section id="backtotop" className="my-8">
-            <div className='flex justify-center items-center'>
-                <Button id="backtotop">Back to top 👆</Button>
-            </div>
-        </section>
-    )
+
+	// No need add any library for using scrollToTop function in this case 😗
+	const scrollToTop = () => {
+		window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+	};
+
+	return (
+		<section id="backtotop" className="my-8">
+			<div className='flex justify-center items-center'>
+				<Button id="backtotop" onClick={scrollToTop}>Back to top 👆</Button>
+			</div>
+		</section>
+	)
 }
 
 export default BackToTop
